68ee908ff7a06c8f9624c633a160f224a0af5c88
[satuhalinen] <satuh9@gmail.com>
2024-01-17 16:49:13 +0200

    add choosing the size of the picture from three options

--- picture_search/README.md
+++ picture_search/README.md
@@ -1,3 +1,3 @@
 # Picture search
 
-Write a word about what you want to see. Click the Search button and you can see the picture about it. Click the Empty button to empty the input field. 
+Write a word about what you want to see. Choose the picture size and click the Search button and you can see the picture about the word you typed. Click the Empty button to empty the input field.
--- picture_search/src/App.jsx
+++ picture_search/src/App.jsx
@@ -4,6 +4,8 @@ import { useState } from "react";
 function App() {
   const [searchInput, setSearchInput] = useState("");
   const [figureUrl, setFigureUrl] = useState("");
+  const defaultSize = "100";
+  const [size, setSize] = useState(defaultSize);
 
   const handleChange = (e) => {
     setSearchInput(e.target.value);
@@ -14,12 +16,22 @@ function App() {
   };
 
   const buttonClick = () => {
-    setFigureUrl(`https://source.unsplash.com/100x100/?${searchInput}`);
+    setFigureUrl(`https://source.unsplash.com/${size}x${size}/?${searchInput}`);
   };
+
+  const sizeHandler = (event) => {
+    setSize(event.target.value);
+  };
+
   return (
     <>
       <button onClick={buttonClick}>Search</button>
       <button onClick={removeButtonClick}>Empty</button>
+      <select value={size} defaultValue={defaultSize} onChange={sizeHandler}>
+        <option value="100">100x100</option>
+        <option value="300">300x300</option>
+        <option value="600">600x600</option>
+      </select>
       <input
         type="text"
         placeholder="Write here"
